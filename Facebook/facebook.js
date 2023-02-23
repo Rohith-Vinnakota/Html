c646d2b7f1e7c536bdfcbb8eff827ad51185906e
Vinnakotarohith <90550145+rohithvinnakota@users.noreply.github.com>
2023-02-23 17:09:18 +0530

    ok

---
+++ Facebook/facebook.js
@@ -0,0 +1,35 @@
+let emailId=document.getElementById("Email");
+let password=document.getElementById("password");
+let errorMessage1=document.getElementById("error_message1");
+let errorMessage2=document.getElementById("error_message2");
+
+function validate(){
+  let matchSymbol="@";
+if((emailId.value.match(matchSymbol) !="@")&(password.value == "")){
+  errorMessage1.style.display="inline-block";
+  errorMessage2.style.display="inline-block";
+  emailId.style.borderColor="red";
+  password.style.borderColor="red";
+}
+else if ((password.value != "")&(emailId.value.match(matchSymbol) !="@")){
+  errorMessage2.style.display="none";
+  errorMessage1.style.display="inline-block";
+  password.style.borderColor="#dddfe2";
+  emailId.style.borderColor="red";
+
+}
+else if ((password.value == "")&(emailId.value.match(matchSymbol) =="@")){
+  errorMessage1.style.display="none";
+  errorMessage2.style.display="inline-block";
+  emailId.style.borderColor="#dddfe2";
+  password.style.borderColor="red";
+
+}
+else{
+  errorMessage1.style.display="none";
+  errorMessage2.style.display="none";
+  password.style.borderColor="#dddfe2";
+  emailId.style.borderColor="#dddfe2";
+}
+
+}
